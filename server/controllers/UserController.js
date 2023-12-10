5adc9526a6e36567fa6d1a809f12df57b4254bc3
Mahmoud Elzanaty <mondyelzanaty@gmail.com>
2023-12-10 21:08:20 +0200

    fixing middle ware

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,12 +1,80 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const productModel = require("../models/productModel");
+const sessionModel = require("../models/SessionModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const User = require('../models/userModel');
 require('dotenv').config();
 
 
 const secretKey = process.env.SECRET_KEY;
-
 const UserController = {
+
+
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ Email: email });
+
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+
+      const isManager = user.is_Manager;
+      const isAgent = user.is_Agent;
+      const isNormalUser = !(isManager || isAgent);
+
+      if (!isManager && !isAgent && !isNormalUser) {
+        return res.status(403).json({ error: 'Access forbidden' });
+      }
+
+      console.log("password: ", user.password);
+      // Check if the password is correct
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(405).json({ message: "incorect password" });
+      }
+
+      const currentDateTime = new Date();
+      const expiresAt = new Date(+currentDateTime + 1800000); // expire in 3 minutes
+      // Generate a JWT token
+      const token = jwt.sign(
+        { user: { userId: user._id, role: user.role } },
+        secretKey,
+        {
+          expiresIn: 3 * 60 * 60,
+        }
+      );
+      let newSession = new sessionModel({
+        userId: user._id,
+        token,
+        expiresAt: expiresAt,
+      });
+      await newSession.save();
+      return res
+        .cookie("token", token, {
+          expires: expiresAt,
+          withCredentials: true,
+          httpOnly: false,
+          SameSite:'none'
+        })
+        .status(200)
+        .json({ message: "login successfully", user });
+    } catch (error) {
+      console.error("Error logging in:", error);
+      res.status(500).json({ message: "Server error" });
+    }
+  },
   createUser: async (req, res) => {
     try {
       const { user_id, Email, is_Agent, is_Manager, Phone_Number, Rate } = req.body;
@@ -36,42 +104,11 @@ const UserController = {
       res.status(500).json({ error: error.message });
     }
   },
-
+ 
   getAllUsers: async (req, res) => {
     try {
       const users = await User.find();
       res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
-
-  login: async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ Email: email });
-
-      if (!user) {
-        return res.status(401).json({ error: 'Invalid credentials' });
-      }
-
-      const isManager = user.is_Manager;
-      const isAgent = user.is_Agent;
-      const isNormalUser = !(isManager || isAgent);
-
-      if (!isManager && !isAgent && !isNormalUser) {
-        return res.status(403).json({ error: 'Access forbidden' });
-      }
-
-      const passwordMatch = await bcrypt.compare(password, user.password);
-
-      if (!passwordMatch) {
-        return res.status(401).json({ error: 'Invalid credentials' });
-      }
-
-      const token = jwt.sign({ userId: user._id, role: user.role }, secretKey, { expiresIn: '1h' });
-
-      res.status(200).json({ token, role: user.role });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
@@ -96,14 +133,15 @@ const UserController = {
         Rate: Rate || 0,
         password,
       });
+      
+
+    
       const savedUser = await newUser.save();
       res.status(201).json(savedUser);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   },
-
-
   getUserById: async (req, res) => {
     try {
       const user = await User.findById(req.params.id);
@@ -113,6 +151,19 @@ const UserController = {
       }
 
       res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  deleteUserById: async (req, res) => {
+    try {
+      const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+      if (!deletedUser) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      res.status(200).json({ message: 'User deleted successfully' });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
@@ -138,20 +189,7 @@ const UserController = {
       res.status(500).json({ error: error.message });
     }
   },
-
-  deleteUserById: async (req, res) => {
-    try {
-      const deletedUser = await User.findByIdAndDelete(req.params.id);
-
-      if (!deletedUser) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-
-      res.status(200).json({ message: 'User deleted successfully' });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
+  
+ 
 };
-
 module.exports = UserController;
