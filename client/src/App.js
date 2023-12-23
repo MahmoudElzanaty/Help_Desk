6236dadf52ccaf7f3e89ff1e3bbd71c43fcf8fe9
Mahmoud Elzanaty <mondyelzanaty@gmail.com>
2023-12-23 21:36:41 +0200

    all working

--- client/src/App.js
+++ client/src/App.js
@@ -1,85 +1,26 @@
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginForm from './components/loginComponent';
+import RegisterForm from './components/registerComponent';
+import TicketForm from './components/createTicketComponent';
+import ViewTicket from './components/viewTicketComponent';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import React, { useState } from 'react';
-
-const RegistrationForm = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    phoneNumber: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('http://localhost:3000/register', {
-      method: 'POST',
-      headers: {
-       'Content-Type': 'application/json',
-       'Authorization': 'user', // Include your token here
-  },
-  body: JSON.stringify(formData),
-});
-
-      if (response.ok) {
-        console.log('Registration successful');
-      } else {
-        console.error('Registration failed');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
-  console.log('Form data:', formData);
-
+function App() {
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </label>
-
-      <br />
-
-      <label>
-        Password:
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </label>
-
-      <br />
-
-      <label>
-        Phone Number:
-        <input
-          type="number"
-          name="phoneNumber"
-          value={formData.phoneNumber}
-          onChange={handleChange}
-        />
-      </label>
-
-      <button type="submit">Register</button>
-    </form>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/create-ticket" element={<TicketForm />} />
+          <Route path="/view-ticket" element={<ViewTicket />} />
+        </Routes>
+      </div>
+    </Router>
   );
-};
+}
 
-export default RegistrationForm;
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
---
+++ client/src/components/loginComponent.js
@@ -0,0 +1,79 @@
+import '../App.css';
+import React, { useState } from 'react';
+
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    Email: '',
+    password: '',
+  });
+  
+
+  const [loginMessage, setLoginMessage] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+        credentials: 'include', 
+      });
+
+      if (response.ok) {
+        setLoginMessage('Login successful');
+      } else {
+        setLoginMessage('Login failed. Please check your email and password.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setLoginMessage('An error occurred during login. Please try again.');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="Email"
+            value={formData.Email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <br />
+
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Login</button>
+      </form>
+
+      {loginMessage && <p>{loginMessage}</p>}
+    </div>
+  );
+};
+
+export default LoginForm;
